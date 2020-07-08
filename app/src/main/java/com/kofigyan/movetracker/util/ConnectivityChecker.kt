cf5a56f624881cf2a0fe474d0d96075ae7ef96a1
KOFI ASANO GYAN <kofigyan2011@gmail.com>
2020-07-08 10:38:06 -0700

    internet connectivity checker util

---
+++ app/src/main/java/com/kofigyan/movetracker/util/ConnectivityChecker.kt
@@ -0,0 +1,23 @@
+package com.kofigyan.movetracker.util
+
+
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.lifecycle.*
+
+
+class ConnectivityChecker(private val connectivityManager: ConnectivityManager) :
+    LifecycleObserver {
+
+    private var monitoringConnectivity = false
+
+    private var _connectedStatus = MutableLiveData<Boolean>()
+    private val connectedStatus: LiveData<Boolean>
+        get() = _connectedStatus
+
+
+
+
+}
