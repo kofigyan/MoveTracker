8ce5ca419270ef897a4f0f983c3409e35ba9efdf
KOFI ASANO GYAN <kofigyan2011@gmail.com>
2020-07-09 04:20:12 -0700

    internet connectivity checker util

--- app/src/main/java/com/kofigyan/movetracker/util/ConnectivityChecker.kt
+++ app/src/main/java/com/kofigyan/movetracker/util/ConnectivityChecker.kt
@@ -3,8 +3,6 @@ package com.kofigyan.movetracker.util
 
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import androidx.lifecycle.*
 
 
@@ -18,14 +16,29 @@ class ConnectivityChecker(private val connectivityManager: ConnectivityManager) 
         get() = _connectedStatus
 
 
-    private val connectivityCallback = object: ConnectivityManager.NetworkCallback(){
+    private val connectivityCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
-            super.onAvailable(network)
+            _connectedStatus.postValue(true)
+
+            connectivityManager.unregisterNetworkCallback(this)
+            monitoringConnectivity = false
         }
 
         override fun onLost(network: Network) {
-            super.onLost(network)
+            _connectedStatus.postValue(false)
         }
 
     }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun stopMonitoringConnectivity() {
+        if (monitoringConnectivity) {
+            connectivityManager.unregisterNetworkCallback(connectivityCallback)
+            monitoringConnectivity = false
+        }
+    }
+
+
+           
+
 }
